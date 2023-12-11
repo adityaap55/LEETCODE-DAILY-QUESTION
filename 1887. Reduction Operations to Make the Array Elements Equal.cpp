be10414b3a5ae130808a4e9b119e0c9b94f1b875
ADITYA PANDEY <142161926+adityaap55@users.noreply.github.com>
2023-12-11 23:15:14 +0530

    Create 1887. Reduction Operations to Make the Array Elements Equal.cpp

---
+++ 1887. Reduction Operations to Make the Array Elements Equal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        int result = 0, curr = 0;
+        for (int i = 1; i < size(nums); ++i) {
+            if (nums[i - 1] < nums[i]) {
+                ++curr;
+            }
+            result += curr;
+        }
+        return result;
+    }
+};
